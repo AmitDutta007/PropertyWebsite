35da8ab48d0472b95f91932d4f94c03d1a0af533
amitdutta <duttaamit356@gmail.com>
2024-08-29 22:47:36 +0530

    Create listing functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import { useSelector } from 'react-redux';
 import {
   FaBath,
   FaBed,
@@ -13,40 +14,43 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from "../components/Contact";
 
 const Listing = () => {
   SwiperCore.use([Navigation]);
-    const [listing, setListing] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [copied, setCopied] = useState(false);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
 
-    
-    const params = useParams();
-    useEffect(() => {
-        const fetchListing = async () => {
-            try {
-                setLoading(true);
-                const res = await fetch(`/api/listing/get/${params.listingId}`);
-                const data = await res.json();
-                if (data.success === false) {
-                    setError(true);
-                    setLoading(false);
-                    return;
-                }
-                setListing(data);
-                setLoading(false);
-                setError(false);
-            } catch (error) {
-                setError(true);
-                setLoading(false);
-            }
-        };
-        fetchListing();
-    }, [params.listingId]);
 
-    return (
-        <main>
+  const [contact, setContact] = useState(false);
+  const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
+
+  return (
+    <main>
       {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
       {error && (
         <p className='text-center my-7 text-2xl'>Something went wrong!</p>
@@ -131,11 +135,17 @@ const Listing = () => {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button onClick={() => setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                Contact landlord
+              </button>
+            )}
+              {contact && <Contact listing={listing}/>}
           </div>
         </div>
       )}
     </main>
-    )
+  )
 }
 
 export default Listing
